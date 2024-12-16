ae66fa44c59cf4bfea7fb329a52326738acd6105
11307703 <11307703@student.pxl.be>
2024-12-16 21:19:45 +0100

    finaal

---
+++ Namenlijst/Namenlijst/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Namenlijst
+{
+    internal class Program
+    {
+        private static string[] namen = new string[4];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===Namen afdrukken===");
+            Console.WriteLine();
+
+            for (int i = 0; i < namen.Length; i++)
+            {
+                Console.Write($"Naam {i+1} :  ");
+                namen[i] = Console.ReadLine();
+            }
+                Array.Sort(namen);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===Namen afgedrukt gesorteerd===");
+            for (int i = 0;i < namen.Length; i++) 
+            {
+                
+                Console.WriteLine($"Naam {i+1} : {namen[i]}");
+                
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
